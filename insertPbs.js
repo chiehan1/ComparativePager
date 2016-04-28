e4491ed85d556ac728296414883ecaef38bf49f9
chiehan1 <chieh.an.hsu@gmail.com>
2016-04-28 10:45:04 +0800

    modify repository folders

--- insertPbs.js
+++ insertPbs.js
@@ -71,7 +71,7 @@ AddTags.prototype.insertTags = function() {
   return this;
 };
 
-var ljTagToDege = new AddTags('./assets/singleFileTest/lijiang', './assets/singleFileTest/dege');
+var ljTagToDege = new AddTags('./takePbTagsHere', './insertPbTagsHere');
 
 var aaa = ljTagToDege.split2Pages().insertTags().textTagTo;
 

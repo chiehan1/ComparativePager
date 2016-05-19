ce9c442d34f5052368643dba2a1feff2c1644d90
chiehan1 <chieh.an.hsu@gmail.com>
2016-05-19 11:53:44 +0800

    modify style

--- insertPbs.js
+++ insertPbs.js
@@ -7,23 +7,26 @@ const flexibleRegex = {
 let getFileName = (folder) => {
   return fs.readdirSync(folder)
     .filter(fileName => ('.' !== fileName[0]))[0];
-}
+};
 
 let replaceWrongTLFFs = (text) => {
   return text.replace(/([\s་])\u0f6aང([\s་])/g, '$1\u0f62ང$2'); // TLFF is Tibetan-Letter-Fixed-Form 
-  // 1. \u0f6aང may be correct in Sanskrit-transliterated Tibetan, but [\s་]\u0f6aང[\s་] is wrong even in Sanskrit-transliterated Tibetan, commented by Karma Lobsang Gurung 2. what's fixed-form-tibetan-letters, see http://unicode.org/charts/PDF/U0F00.pdf
-} 
+  // 1. \u0f6aང may be correct in Sanskrit-transliterated Tibetan,
+  //    but [\s་]\u0f6aང[\s་] is wrong even in Sanskrit-transliterated Tibetan, 
+  //    commented by Karma Lobsang Gurung 
+  // 2. what's fixed-form-tibetan-letters, see http://unicode.org/charts/PDF/U0F00.pdf
+};
 
 let getText = (folder) => {
   let fileName = getFileName(folder);
   let originalText = fs.readFileSync(folder + '/' + fileName, 'utf8');
 
   return replaceWrongTLFFs(originalText);
-}
+};
 
 let removeNonPbTags = (text) => {
   return text.replace(/<(?!pb).*?>/g, '');
-}
+};
 
 let split2Pages = (text, tagFirstLetter) => {
   let pages = removeNonPbTags(text).replace(/<(?!pb).*?>/g, '')
@@ -40,7 +43,7 @@ let split2Pages = (text, tagFirstLetter) => {
   });
 
   return pageObjs;
-}
+};
 
 let split2Syls = (text) => {
   let syls = modifyText(text).split('་');
@@ -52,12 +55,12 @@ let split2Syls = (text) => {
       return syl;
     }
   });
-}
+};
 
 let modifyText = (text) => {
   return text.replace(/(\r?\n)+/g, '་')
     .replace(/[༆༈།༎༏༐༑༒་ ]+/g, '་');
-}
+};
 
 let insertPbTags = (refFolder, targetFolder) => {
   let refTagFirstLetter = getFileName(refFolder)[0];
@@ -73,6 +76,7 @@ let insertPbTags = (refFolder, targetFolder) => {
     let matchRegex = syls[0] + sylSeparator;
     let lastMatchRegex = '';
     let totalSylsN = syls.length;
+
 
     for (let i = 1; i < totalSylsN; i++) {
       let regex = new RegExp(matchRegex, 'g');
@@ -110,7 +114,7 @@ let insertPbTags = (refFolder, targetFolder) => {
   });
 
   return targetText;
-}
+};
 
 let insertedPbText = insertPbTags('./takePbTagsHere', './insertPbTagsHere');
 
